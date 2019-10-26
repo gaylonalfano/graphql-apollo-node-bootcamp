ced1d129beb754a59be214191d4e9370f704bc26
gaylonalfano <gaylon.alfano@gmail.com>
2019-10-26 13:32:03 +0800

    Created a very basic dummy subscription to get introduced to the flow and syntax. Subscriptions behind the scenes use web sockets to create real-time connections from the server to the client.

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -3,6 +3,17 @@ const Subscription = {
     subscribe(obj, args, { pubsub }, info) {
       // Set up our subscription
       let count = 0;
+
+      // Increment by 1 every second
+      setInterval(() => {
+        count++;
+        pubsub.publish('count', {
+          count
+        });
+      }, 1000);
+
+      // Return asyncIterator with channel name
+      return pubsub.asyncIterator('count');
     }
   }
 };
